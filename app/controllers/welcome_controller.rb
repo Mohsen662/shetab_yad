c0546daa61fc28b7037cc3611625a2cb811d0bec
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-03-07 17:35:21 +0330

    about us page added and styled

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :about_us]
+  
   def index
   end
+
+  def about_us
+  end
+  
 end
